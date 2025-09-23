610982ad27ff085b7a733c396f2d443aa122e5cb
Toqa Essam <Tokaessam81@users.noreply.github.com>
2025-09-24 01:02:41 +0300

    AddingTokenForProduct

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { JwtInterceptor } from '../src/app/Models/interceptors/jwt.interceptor.ts';
+import { AuthService } from '../src/app/Models/services/auth.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
     ...appConfig.providers,
-    importProvidersFrom(HttpClientModule, ReactiveFormsModule)
+    importProvidersFrom(HttpClientModule, ReactiveFormsModule),
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, // تسجيل الـ interceptor
   ]
 });
